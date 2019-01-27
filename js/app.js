006aec2682c28a1501b9b32402c4633977adfacb
Rina Toyoshiba <rtoyoshiba@gmail.com>
2019-01-26 23:21:31 -1000

    main event

--- js/app.js
+++ js/app.js
@@ -7,14 +7,19 @@ The function will add the following content inside the paragraph with the id of 
 */
 
 var bliss = "Let's put some happy little bushes on the other side now. Anything you want to do you can do here. Trees cover up a multitude of sins. If you don't like it - change it. It's your world. Maybe there's a happy little waterfall happening over here."
-
+function moreTrees(){
+    nirvana.innerHTML = bliss;
+}
 
 //2. Less is More
 /*Create a function named `lessContent` that will invoke the function after clicking on the button.
 
 The function will hide the contents in the pargraph with the id of `less`.*/
 
-
+function lessContent(){
+    // less.innerHTML = ''; This worked too though.
+    less.style.display = 'none';
+}
 
 //3. Edward (エドワード)
 /*Create a function named `bling` that will change the text in the paragraph of `worm` after hovering over it. Use the style guide below.
@@ -26,13 +31,26 @@ border: 3px dotted rosybrown
 padding: 10px
 */
 
-
+function bling(){
+    worm.style.fontSize = '28px';
+    worm.style.fontColor = '#33cc33';
+    worm.style.border = '3px dotted rosybrown';
+    worm.style.padding = '10px';
+}
 
 //4. Beet Cakes by Dre
 /*Add an event listener to the `recipe` button that will display the missing ingredient in the paragraph of `ingredient` after clicking on the button*/
 
 var missing = "1 Big ol' beet";
 
+recipe.addEventListener('click', getRecipe);
+
+function getRecipe(){
+        var liElem = document.createElement('li');
+        liElem.className = 'mix';
+        liElem.innerHTML = missing;
+        ingredient.appendChild(liElem);
+}
 
 
 //5. Pug Life
@@ -41,11 +59,28 @@ var missing = "1 Big ol' beet";
 // Use this for the new image:
 // 'https://media.giphy.com/media/12pJ8OxSWwO86Y/giphy.gif'
 
+puggy.addEventListener('mouseover',newImage);
 
+function newImage(){
+    if(puggy.src === 'https://bit.ly//2Rh0vm4'){
+        puggy.src = 'https://media.giphy.com/media/12pJ8OxSWwO86Y/giphy.gif';
+    }else{
+        puggy.src = 'https://bit.ly//2Rh0vm4'
+    }
+}
 
 //6. DevLeague Lunch Wagon
 /*Add an event listener to the `menu` div element that will display and hide the dessert item after clicking on 'Today's Special'*/
 
+menu.addEventListener('click', dessertItem);
+
+function dessertItem(){
+    if(dessert.style.display === 'block'){
+        dessert.style.display = 'none';
+    }else{
+        dessert.style.display = 'block';
+    }
+}
 
 
 //7. The Purple Monster
@@ -59,13 +94,43 @@ var grimace = {
     hobbies: ['knitting', 'scrapbooking', 'mixed martial arts']
 };
 
+bio.addEventListener('click', aboutGrimace);
+
+function aboutGrimace(){
+    var nameElem = document.createElement('div');
+    nameElem.id = 'ProfileName';
+    nameElem.innerHTML = 'Name:' + grimace.name;
+    bio.appendChild(nameElem);
+
+    var signElem = document.createElement('div');
+    signElem.id = 'ProfileSign';
+    signElem.innerHTML = 'Sign:' + grimace.sign;
+    bio.appendChild(signElem);
+
+    var hobbiesElem = document.createElement('div');
+    hobbiesElem.id = 'ProfileHobbies';
+    hobbiesElem.innerHTML = 'Hobbies:' + grimace.hobbies.toString('');
+    bio.appendChild(hobbiesElem);
+}
 
 //8. Fortune Cookie
 /*Add an event listener to the `fortune` button that will generate a random message in the `showFortune` h3 element after clicking on the button*/
 
 var messages = ["That wasn't chicken.", "The fortune you seek is in another cookie.", "Tell your friends about DevLeague Prep.", "You will be doing many events tonight.", "Don't let statistics do a number on you.", "Buy your instructor a drink and receive double fortune.", "Is a turtle without a shell naked or homeless?"];
 
+fortune.addEventListener('click', getFortune);
+
+function getFortune(){
+    var random = Math.floor(Math.random()*messages.length);
+    var ranMsg = messages[random];
+    showFortune.innerHTML = ranMsg;
+}
 
 //9. Open Sesame
 /*Add an event listener to the `secret` input element that will capitalize letter inputs in the form. Hint, you'll be using the keyup event*/
 
+secret.addEventListener('keyup', myPassword);
+
+function myPassword(){
+    secret.value = secret.value.toUpperCase();
+}
